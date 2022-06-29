c8bc60904818d0db5e725fd5f90fd2180190b11a
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-06-28 20:50:13 -0400

    Added two working controllers for POST at /users/add and GET at /users/test in the api

--- api/src/main/java/com/budgetapp/api/UserController.java
+++ api/src/main/java/com/budgetapp/api/UserController.java
@@ -1,20 +1,22 @@
 package com.budgetapp.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+    @Autowired
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-
-    // Creation methods
-    @PostMapping("/register")
+    // CREATE methods
+    @PostMapping("/add") // Map ONLY POST Requests
     public @ResponseBody String addNewUser (@RequestParam String email
             , @RequestParam String password, @RequestParam String firstName, @RequestParam String lastName) {
         // @ResponseBody means the returned String is the response, not a view name
@@ -29,13 +31,14 @@ public class UserController {
         return "Saved";
     }
 
-    // Retrieval methods
-    @GetMapping("/{userId}")
-    public User getUser(@PathVariable Long userId) {
-        return this.userRepository.findById(userId).get();
+    // READ methods
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("THIS SHOULD WORK.");
+        return "Test.";
     }
 
-    // Update methods
+    // UPDATE methods
 
-    // Deletion methods
+    // DELETE methods
 }
