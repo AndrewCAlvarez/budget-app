16febcc6a02a5711b3703b4c6191e8d8a058a196
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-09-06 17:53:35 -0400

    Roll back api security temporarily

--- api/src/main/java/com/budgetapp/api/BudgetAppApiApplication.java
+++ api/src/main/java/com/budgetapp/api/BudgetAppApiApplication.java
@@ -2,11 +2,11 @@ package com.budgetapp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
+//@EnableWebSecurity
 public class BudgetAppApiApplication {
 
 	public static void main(String[] args) {
