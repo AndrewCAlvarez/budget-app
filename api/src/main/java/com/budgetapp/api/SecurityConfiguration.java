77ca92693b5e8798a2845871326e483de039bb0e
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-09-15 17:04:09 -0400

    Override UserDetailsService & CustomUserDetails
    NOTE: Lots of hard-coded values here that need to be changed later.

--- api/src/main/java/com/budgetapp/api/SecurityConfiguration.java
+++ api/src/main/java/com/budgetapp/api/SecurityConfiguration.java
@@ -1,24 +1,26 @@
 package com.budgetapp.api;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     // Authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication()
-                    .withUser("user")
-                    .password("password")
-                    .roles("USER");
+            auth.userDetailsService(userDetailsService);
     }
 
     @Bean
