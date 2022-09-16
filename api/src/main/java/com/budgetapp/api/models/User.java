d85397413b0404c7532be0f5d0488ec262a10822
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-09-15 20:52:56 -0400

    Implement new user for api

---
+++ api/src/main/java/com/budgetapp/api/models/User.java
@@ -0,0 +1,15 @@
+package com.budgetapp.api.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+}
